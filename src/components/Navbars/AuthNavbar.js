a08316e43bb07982ebbd733ab81d463d36440e0a
Prithvi Vittaladevaram <prithvi.sai@outlook.com>
2022-12-03 13:56:30 +0530

    cmt-authnavbar

--- src/components/Navbars/AuthNavbar.js
+++ src/components/Navbars/AuthNavbar.js
@@ -9,7 +9,7 @@ import PagesDropdown from "components/Dropdowns/PagesDropdown.js";
 export default function Navbar(props) {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
-    <div>
+    <>
       <nav className="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
@@ -19,29 +19,9 @@ export default function Navbar(props) {
             >
               Home Page
             </Link>
-<<<<<<< HEAD
-            <button
-              className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-              type="button"
-              onClick={() => setNavbarOpen(!navbarOpen)}
-            >
-              <i className="text-white fas fa-bars"></i>
-            </button>
           </div>
-          <div
-            className={
-              "lg:flex flex-grow items-center bg-white lg:bg-opacity-0 lg:shadow-none" +
-              (navbarOpen ? " block rounded shadow-lg" : " hidden")
-            }
-            id="example-navbar-warning"
-          >
-
-          </div>
-=======
-            </div>
->>>>>>> a4e0676208688e8b785996a160690d4d57aa90ed
         </div>
       </nav>
-    </div>
+    </>
   );
 }
